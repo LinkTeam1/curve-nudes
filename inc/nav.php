fc56d8e20130d8f1be0c4e64a4bd1bac5498d994
ali neji <alinj161@gmail.com>
2023-12-15 02:17:57 +0100

    Initial commit

---
+++ inc/nav.php
@@ -0,0 +1,66 @@
+<!-- ? Preloader Start -->
+<div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/loder.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader Start-->
+    <header>
+        <!-- Header Start -->
+        <div class="header-area ">
+            <div class="main-header header-sticky">
+                <div class="container-fluid">
+                    <div class="menu-wrapper d-flex align-items-center justify-content-between">
+                        <div class="header-left d-flex align-items-center">
+                            <!-- Logo -->
+                            <div class="logo">
+                                <a href="."><img src="assets/img/logo/logo.png" alt=""></a>
+                            </div>
+                            <!-- Main-menu -->
+                            <div class="main-menu  d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li id="itemsnav"><a href=".">Home</a></li> 
+                                        <li id="itemsnav"><a href="shop.php">shop</a></li>
+                                        <li id="itemsnav"><a href="about.php">About</a></li>
+                                        <li id="itemsnav"><a href="contact.php">Contact</a></li>
+                                    </ul>   
+                                </nav>
+                            </div>   
+                        </div>
+                        <div class="header-right1 d-flex align-items-center">
+                            <!-- Social -->
+                            <div class="header-social d-none d-md-block">
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="https://bit.ly/sai4ull"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                            </div>
+                            <!-- Search Box -->
+                            <div class="search d-none d-md-block">
+                                <ul class="d-flex align-items-center">
+                                  
+                                    <li>
+                                        <div class="card-stor">
+                                            <img src="assets/img/gallery/card.svg" alt="">
+                                            <span>0</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+    </header>
+    
---
+++ info/new-arrival.php
@@ -0,0 +1,111 @@
+<!--? New Arrival Start -->
+<div class="new-arrival">
+    <div class="container">
+        <!-- Section tittle -->
+        <div class="row justify-content-center">
+            <div class="col-xl-7 col-lg-8 col-md-10">
+                <div class="section-tittle mb-60 text-center wow fadeInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                    <h2>new<br>arrival</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                <div class="single-new-arrival mb-50 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">
+                    <div class="popular-img">
+                        <img src="assets/img/gallery/arrival1.png" alt="">
+                        <div class="favorit-items">
+                            <!-- <span class="flaticon-heart"></span> -->
+                            <img src="assets/img/gallery/favorit-card.png" alt="">
+                        </div>
+                    </div>
+                    <div class="popular-caption">
+                        <h3><a href="product_details.html">Kiss me</a></h3>
+                        <div class="rating mb-10">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>70 € </span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                <div class="single-new-arrival mb-50 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">
+                    <div class="popular-img">
+                        <img src="assets/img/gallery/arrival2.png" alt="">
+                        <div class="favorit-items">
+                            <!-- <span class="flaticon-heart"></span> -->
+                            <img src="assets/img/gallery/favorit-card.png" alt="">
+                        </div>
+                    </div>
+                    <div class="popular-caption">
+                     <h3><a href="product_details.html">Athena</a></h3>
+                     <div class="rating mb-10">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <span>50 €</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+            <div class="single-new-arrival mb-50 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
+                <div class="popular-img">
+                    <img src="assets/img/gallery/arrival3.png" alt="">
+                    <div class="favorit-items">
+                        <!-- <span class="flaticon-heart"></span> -->
+                        <img src="assets/img/gallery/favorit-card.png" alt="">
+                    </div>
+                </div>
+                <div class="popular-caption">
+                 <h3><a href="product_details.html">Athena</a></h3>
+                 <div class="rating mb-10">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <span>70 €</span>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+        <div class="single-new-arrival mb-50 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">
+            <div class="popular-img">
+                <img src="assets/img/gallery/arrival4.png" alt="">
+                <div class="favorit-items">
+                    <!-- <span class="flaticon-heart"></span> -->
+                    <img src="assets/img/gallery/favorit-card.png" alt="">
+                </div>
+            </div>
+            <div class="popular-caption">
+             <h3><a href="product_details.html">Im gorgeous</a></h3>
+             <div class="rating mb-10">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <span>60 €</span>
+        </div>
+    </div>
+</div>
+
+</div>
+<!-- Button -->
+<div class="row justify-content-center">
+    <div class="room-btn">
+        <a href="shop.php" class="border-btn">Browse More</a>
+    </div>
+</div>
+</div>
+</div>
+<!--? New Arrival End -->
